47c069ed1cdf6ba3dd9c4d2144b556f9ad891f4b
Luan Braga <luanbraga.lb@gmail.com>
2021-05-31 09:56:49 -0300

    modify m-o-c.component.ts

--- src/app/meu-outro-componente/meu-outro-componente.component.ts
+++ src/app/meu-outro-componente/meu-outro-componente.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { AlunosService } from '../services/alunos.service';
 
@@ -44,8 +44,9 @@ export class MeuOutroComponenteComponent implements OnInit {
       const url = `https://api.github.com/search/repositories`;
 
       const params = new HttpParams().set('q', this.searchText);
+      const headers = new HttpHeaders().set('Content-Type', 'text/html');
       
-      this.http.get<gitHubResponse>(url, {params}).subscribe(
+      this.http.get<gitHubResponse>(url, {params, headers}).subscribe(
         response => {
           this.projects = response.items;
         }
